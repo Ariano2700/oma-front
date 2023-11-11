e2b7f5d01ba7fa3c854fd2dcefa598d7b34f0ab0
Ariano2700 <kawuai123loco123@gmail.com>
2023-11-11 02:04:22 -0500

    foto de perfil visible XD

--- user_profile/showContent.js
+++ user_profile/showContent.js
@@ -24,7 +24,6 @@ async function obtenerDNI() {
     console.error(error);
   }
 }
-
 async function obtenerDatos(dni) {
   try {
     const response = await fetch(
@@ -94,15 +93,40 @@ async function mostrarDatosTabla() {
   const direccionSpan = document.getElementById("direccion");
   const emailSpan = document.getElementById("email");
   const nombreUsuario = document.getElementById("nombreUsuario");
+  const avatarImage = document.getElementById("avatar-image");
 
   const dni = await obtenerDNI();
   const data = await obtenerTodosDatos(dni);
   if (data) {
-    const { nombre, rol, username, email, apellido, telefono, direccion } =
-      data;
-    console.log("hola");
+    const {
+      nombre,
+      rol,
+      username,
+      email,
+      apellido,
+      telefono,
+      direccion,
+      fotoPerfil,
+    } = data;
 
-    nombreUsuario.innerHTML = `<p>${nombre}, ${apellido}</p>`
+    //avatarImage.src = `data:image/jpg;base64,${fotoPerfil}`
+    console.log(`data:image/jpg;base64,${fotoPerfil}`)
+    if (fotoPerfil && fotoPerfil.length > 0) {
+      // Convertir la cadena base64 a Blob
+      const byteCharacters = atob(fotoPerfil);
+      const byteNumbers = new Array(byteCharacters.length);
+      for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      const blob = new Blob([byteArray], { type: "image/jpeg" });
+
+      // Crear una URL de datos y establecerla como src de la imagen
+      const imageURL = URL.createObjectURL(blob);
+      avatarImage.src = imageURL;
+    }
+
+    nombreUsuario.innerHTML = `<p>${nombre}, ${apellido}</p>`;
     usernameSpan.innerHTML = `<p>${username}</p>`;
     rolSpan.innerHTML = `<p>${rol}</p>`;
     telefonoSpan.innerHTML = `<p>${telefono}</p>`;
@@ -136,6 +160,7 @@ async function obtenerTodosDatos(dni) {
       const email = data.email;
       const telefono = data.telefono;
       const direccion = data.direccion;
+      const fotoPerfil = data.fotoPerfil;
 
       const roleName = await obtenerRol(rol);
       return {
@@ -146,6 +171,7 @@ async function obtenerTodosDatos(dni) {
         nombre: nombre,
         telefono: telefono,
         direccion: direccion,
+        fotoPerfil: fotoPerfil,
       };
     }
   } catch (error) {
@@ -154,3 +180,71 @@ async function obtenerTodosDatos(dni) {
   }
 }
 mostrarDatosTabla();
+
+////////////////////////////////
+/* ESCOGER IMG DE PERFIL / PORTADA */
+async function chooseAvatarImage() {
+  const avatarInput = document.getElementById("avatar-input");
+  avatarInput.click();
+
+  avatarInput.addEventListener("change", async function () {
+    const avatarImage = document.getElementById("avatar-image");
+    const selectedFile = avatarInput.files[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = async function (e) {
+        avatarImage.src = e.target.result;
+
+        // Utilizar la función uploadAvatar que es asíncrona
+        await uploadAvatar(selectedFile);
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
+  });
+}
+
+async function uploadAvatar(file) {
+  try {
+    const dni = await obtenerDNI(); // Reemplaza con el valor correcto del trabajador
+    const formData = new FormData();
+    formData.append("fotoPerfil", file);
+
+    const response = await fetch(
+      `http://localhost:8080/api/trabajador/actualizar/foto/perfil/${dni}`,
+      {
+        method: "PATCH",
+        // No establezcas Content-Type, dejar que FormData lo maneje automáticamente
+        // headers: {
+        //     "Content-Type": "multipart/form-data",
+        // },
+        body: formData,
+      }
+    );
+
+    const data = await response.json();
+    console.log("Imagen subida exitosamente", data);
+  } catch (error) {
+    console.error("Error al subir la imagen", error);
+  }
+}
+///////////////////////////////////////
+/*function choosePortadaImage() {
+  const portadaInput = document.getElementById("portada-input");
+  portadaInput.click();
+
+  portadaInput.addEventListener("change", function () {
+    const portadaImage = document.getElementById("portada-image");
+    const selectedFile = portadaInput.files[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        portadaImage.src = e.target.result;
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
+  });
+}*/
