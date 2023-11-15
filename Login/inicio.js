921392158e8cb77b3a1f1b98c6bf363289f219ec
Ariano2700 <kawuai123loco123@gmail.com>
2023-11-15 15:37:22 -0500

    Validacion triple para iniciar sesion

--- Login/inicio.js
+++ Login/inicio.js
@@ -7,25 +7,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById("password").value;
     const title = "¡Debes completar tus datos!";
     const text = "Rellena el formulario para completar tu inicio de sesión";
-
+    /*if(username.includes("@") && username.includes(".com")){
+      console.log("Es un correo");
+    }else if(/^\d{8}$/.test(username)){
+      console.log("Es un dni");
+    }else{
+      console.log("Es un username")
+    }*/
     username === "" || password === ""
       ? alertNoComplete(title, text)
       : await verificarExistencia(username, password);
   });
 
-  async function verificarExistencia(email, password) {
+  async function verificarExistencia(param, password) {
     const response = await fetch(
-      `http://localhost:8080/api/trabajador/verificar/email?email=${email}`
+      `http://localhost:8080/api/trabajador/verificar/existencia/cdu?param=${param}`
     );
     const data = await response.json();
     const valido = data.Existe;
-    const title = "¡Email incorrecto!";
-    const text = "Verifique si la direccion email es correcta o no existe";
+    const title = "¡Email, username o DNI incorrecto!";
+    const text = "Verifique si el dato ingresado es correcta o no existe";
     //console.log(valido);
     valido === true
-      ? iniciarSesion(email, password)
-      : console.log("El correo no existe en la bd"),
-      alertNoComplete(title, text);
+      ? iniciarSesion(param, password)
+      : alertNoComplete(title, text)//falta colocar ',' para usar el console log
+      //console.log("El correo no existe en la bd");
   }
   async function obtenerRol(rol) {
     try {
@@ -42,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (data) {
         const nombreRol = data.nombre;
-        console.log(nombreRol);
+        //console.log(nombreRol);
         return nombreRol;
       } else {
         throw new Error("No se encontró el rol");
@@ -73,9 +79,9 @@ document.addEventListener("DOMContentLoaded", function () {
       if (data.password === password) {
         const rol = data.idRol;
         const dni = data.dni;
-        console.log(rol, "con dni", dni);
+        //console.log(rol, "con dni", dni);
         const roleName = await obtenerRol(rol);
-        console.log(roleName);
+        //console.log(roleName);
         await iniciarSesionEnPHP(username, dni);
         await alertSuccess(roleName);
         setTimeout(() => {
@@ -107,11 +113,11 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       const responseData = await response.text();
-      console.log("Respuesta de sesionStart.php:", responseData);
+      //console.log("Respuesta de sesionStart.php:", responseData);
 
       // Intenta analizar la respuesta como JSON
       const data = JSON.parse(responseData);
-      console.log("dni:", dni);
+      //console.log("dni:", dni);
       if (data.username === username) {
         await guardarDatos(dni);
         console.log("Sesión PHP iniciada con éxito");
@@ -138,10 +144,10 @@ document.addEventListener("DOMContentLoaded", function () {
         );
       }
       const data = await response.json();
-      console.log(data);
+      //console.log(data);
 
       if (data.dni === dni) {
-        console.log("dni guardado", dni);
+        //console.log("dni guardado", dni);
       } else {
         console.error("Error al guardar el dni");
       }
